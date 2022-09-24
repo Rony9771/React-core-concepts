57d7c11e30be6ca99d228df3dcb044e03305e21e
Rony <rony.ahmed9771@gmail.com>
2022-09-24 23:04:58 +0600

    react core concepts

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Person name="M.S Dhoni" Profession="Cricketer"></Person>
+      <Person name="Shakib Al Hasan" Profession="Businessman"></Person>
+        
+     </div>  
   );
+} 
+
+function Person (props){
+  console.log(props)
+  return (
+    <div className='container'>
+      <h1>{props.name}</h1>
+      <p>{props.Profession}</p>
+    </div>
+  )
 }
+
+        
+        
+        
+
+
 
 export default App;

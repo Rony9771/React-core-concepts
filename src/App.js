88b83e57890d5b6e69e3adafc2453f3dca13184d
Rony <rony.ahmed9771@gmail.com>
2022-09-24 23:56:13 +0600

    add more data dynamically

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 // import logo from './logo.svg';
 import './App.css';
 
+const heros = [
+  {name:'Salman Khan', age: 52 , salary:'100cr'},
+  {name:'Sharukh Khan', age: 50 , salary:'10cr'}
+]
+
 
 function App() {
   const players =['Tamim', 'Bijoy', 'Shakib', 'Mushi', 'Liton']
@@ -9,8 +14,11 @@ function App() {
       {
         players.map(player => <h1>{player}</h1>)
       }
-      {
+      {/* {
         players.map(player => <Person name = {player}></Person>)
+      } */}
+      {
+        heros.map(hero => <Person name={hero.name} age={hero.age}></Person>)
       }
 
       <Person name="M.S Dhoni" Profession="Cricketer"></Person>
@@ -25,7 +33,10 @@ function Person (props){
   return (
     <div className='container'>
       <h1>{props.name}</h1>
+      <p>{props.age}</p>
       <p>{props.Profession}</p>
+      
+
     </div>
   )
 }
